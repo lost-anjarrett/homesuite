66a641efde440813f60e3faef4bc7ec078d09e2e
poncif-true <dev@poncif.fr>
2020-02-08 18:40:48 +0100

    fix

--- src/Migrations/Version20200208173152.php
+++ src/Migrations/Version20200208173152.php
@@ -22,17 +22,17 @@ final class Version20200208173152 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(1, 'bread', 30);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(2, 'cooked meal', 100);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(3, 'fruit / veg.', 365);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(4, 'meat (chicken)', 180);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(5, 'meat (pork,lamb,veal)', 200);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(6, 'meat (beef,poultry)', 240);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(7, 'ground meat', 75);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(8, 'fish, crustacean', 90);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(9, 'pastry', 60);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(10, 'cake', 90);");
-        $this->addSql("INSERT INTO homesuite.freezer_item_category (id, name, default_validity) VALUES(11, 'cheese, butter', 90);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(1, 'bread', 30);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(2, 'cooked meal', 100);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(3, 'fruit / veg.', 365);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(4, 'meat (chicken)', 180);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(5, 'meat (pork,lamb,veal)', 200);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(6, 'meat (beef,poultry)', 240);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(7, 'ground meat', 75);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(8, 'fish, crustacean', 90);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(9, 'pastry', 60);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(10, 'cake', 90);");
+        $this->addSql("INSERT INTO freezer_item_category (id, name, default_validity) VALUES(11, 'cheese, butter', 90);");
     }
 
     public function down(Schema $schema) : void
